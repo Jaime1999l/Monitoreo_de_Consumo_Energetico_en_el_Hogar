d942ea20d2d21f335b7a73cdf2a235ac46efdfbc
NicolasJRuax <160480830+NicolasJRuax@users.noreply.github.com>
2024-10-01 18:28:41 +0200

    Initial commit

---
+++ app/src/test/java/com/example/monitoreo_de_consumo_energtico_en_el_hogar/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.monitoreo_de_consumo_energtico_en_el_hogar;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Monitoreo_de_Consumo_Energético_en_el_Hogar"
+include(":app")
+ 
