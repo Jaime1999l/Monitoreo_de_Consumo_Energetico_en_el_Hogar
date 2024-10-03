6d9052b111e5d0180a54461ff486b7416fbb6574
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-10-03 21:45:35 +0200

    -

--- app/src/main/java/com/example/monitoreo_de_consumo_energtico_en_el_hogar/activity/RoomMonitorActivity.java
+++ app/src/main/java/com/example/monitoreo_de_consumo_energtico_en_el_hogar/activity/RoomMonitorActivity.java
@@ -1,4 +1,5 @@
 package com.example.monitoreo_de_consumo_energtico_en_el_hogar.activity;
 
 public class RoomMonitorActivity {
+    // Representacion de graficos que muestren los datos del consumo de las habitaciones asi como el promedio de consumo de cada habitacion, el numero de habitaciones y pasillos etc (coger datos de las colecciones "habitaciones" y "pasillos" en firebase). Los pasillos no tienen consumo energetico.
 }
